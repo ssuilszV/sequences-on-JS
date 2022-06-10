3eb488a447c9711ccab2a7d731efc1cba71656d5
Louis H <94133351+ssuilszV@users.noreply.github.com>
2022-06-10 15:01:31 -0300

    Add files via upload

---
+++ sequencia_1.js
@@ -0,0 +1,15 @@
+//Essas são sequências de código, funcionam como  frases em um texto,
+
+//Essa é a primeira sequência (1 unidade)
+console.log("Salve!");
+
+//Essa é nosso segundo exemplo, porém ainda é apenas uma sequência de código, mas apenas "quebrado"
+console
+.
+log(
+"Belezããn?"
+);
+
+//Nesse terceiro exemplo, temos 3 sequências de código, nas quais estão organizadas e prontas para serem usadas
+console.log("Tranquildade, cria");
+console.log("Nice brow");
---
+++ sequencia_2.js
@@ -0,0 +1,18 @@
+//     **-Depois de códificar, organizei melhor o espaço ultilizando "\n" que é uma quebra de linha-**
+
+
+
+
+//Quando o console.log() é escrito apenas dessa forma{sem aspas internas [simples ou dupla]} podemos obter resultados de somas e provavelmente de variaveis dentro, subtraindo, somando, cortando outras.
+console.log("\n", 1 + 3);
+
+//Quando escrevemos com aspas (simples ou dupla), é possivel escrever QUALQUER coisa lá dentro, que não será "entendido" como sequência de código.
+console.log("\n 1 + 3");
+
+//Assim como posso usar as duas para indicar uma frase e uma somatória de alguns numeros. Lembrando que, para separar o que será escrito no console, teremos que seperar usando umc vírgula como é o exemplo abaixo:)
+console.log("\n A somatória de 1 + 3 é =", 1 + 3 )
+
+//Exemplos:
+console.log("Passo #1");
+console.log("Passo #2");
+console.log("Passo #3");
